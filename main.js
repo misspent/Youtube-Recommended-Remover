70b51810f85f05912da9f134a07aa86a9c5eaebf
misspent <78914154+misspent@users.noreply.github.com>
2022-11-18 14:00:42 +0000

    Quick fix

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        I'm not interested in this fucking YouTube video!
 // @namespace   http://www.iamnotinterestedinthisfuckingyoutubevideo.com
-// @version     1.0
+// @version     2.0
 // @description I'm not interested in this fucking YouTube video!
 // @match       *://www.youtube.com/*
 // @run-at      document-end
@@ -10,7 +10,7 @@
 
 (function() {
     'use strict';
-    if (/youtube\.com\/?$/.test(location.href)) {
+    if (/youtube\.com\//.test(location.href)) {
         console.log("Youtube home page detected!");
 
         setInterval((function () {
@@ -22,7 +22,13 @@
                 let cell = $(this);
                 let temp = cell.find("ytd-thumbnail");
                 temp.hover(function(e){
-                    if(e.ctrlKey) {
+                    if(e.shiftKey) {
+                        $(this).closest("ytd-rich-item-renderer").find("button.style-scope.yt-icon-button").click();
+                        setTimeout(function(){
+                            $("yt-formatted-string:contains('Recommend less like this')").click();
+                        }, 50);
+                    }
+                    if(e.shiftKey) {
                         $(this).closest("ytd-rich-item-renderer").find("button.style-scope.yt-icon-button").click();
                         setTimeout(function(){
                             $("yt-formatted-string:contains('Not interested')").click();
@@ -30,31 +36,6 @@
                     }
                     else if(e.altKey) {
                         $(this).closest("ytd-rich-item-renderer").find("button.style-scope.yt-icon-button").click();
-                        setTimeout(function(){
-                            $(`yt-formatted-string:contains("Don't recommend channel")`).click();
-                        }, 50);
-                    }
-                });
-            });
-            cells.addClass("fucked");
-        }), 2000);
-    }
-
-    if (/youtube\.com\/watch/.test(location.href)) {
-        setInterval((function () {
-            let cells = $("ytd-compact-video-renderer.style-scope.ytd-item-section-renderer:not(.fucked)");
-            cells.each(function(){
-                let cell = $(this);
-                let temp = cell.find("ytd-thumbnail");
-                temp.hover(function(e){
-                    if(e.ctrlKey) {
-                        $(this).closest("ytd-compact-video-renderer").find("button.style-scope.yt-icon-button").click();
-                        setTimeout(function(){
-                            $("yt-formatted-string:contains('Not interested')").click();
-                        }, 50);
-                    }
-                    else if(e.altKey) {
-                        $(this).closest("ytd-compact-video-renderer").find("button.style-scope.yt-icon-button").click();
                         setTimeout(function(){
                             $(`yt-formatted-string:contains("Don't recommend channel")`).click();
                         }, 50);
