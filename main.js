af3bb06c67bc65821c32fc75b86ab9bf1d62b0b7
misspent <78914154+misspent@users.noreply.github.com>
2023-10-12 16:43:57 +0100

    Update main.js
    Hides video in subscriptions section with "shift" key

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         I'm not interested in this fucking YouTube video!
-// @namespace    http://www.iamnotinterestedinthisfuckingyoutubevideo.com
 // @version      2.0
-// @description  I'm not interested in this fucking YouTube video!
+// @description  Removes videos from home page and hides videos from subscriptiion page when you press the allocated keybind
+// @namespace    0x7FFFFFFFFFFFFFFF & Misspent
 // @icon         https://www.youtube.com/s/desktop/03f86491/img/favicon.ico
 // @match        *://www.youtube.com/*
 // @run-at       document-end
@@ -23,19 +23,25 @@
                 let cell = $(this);
                 let temp = cell.find("ytd-thumbnail");
                 temp.hover(function(e){
-                    if(e.shiftKey) {
+                    if(e.shiftKey) { // Do not recommened in home page section v2
                         $(this).closest("ytd-rich-item-renderer").find("button.style-scope.yt-icon-button").click();
                         setTimeout(function(){
                             $("yt-formatted-string:contains('Recommend less like this')").click();
                         }, 50);
                     }
-                    if(e.shiftKey) {
+                    if(e.shiftKey) { // Do not recommened in home page section v1
                         $(this).closest("ytd-rich-item-renderer").find("button.style-scope.yt-icon-button").click();
                         setTimeout(function(){
                             $("yt-formatted-string:contains('Not interested')").click();
                         }, 50);
                     }
-                    else if(e.altKey) {
+                    if(e.shiftKey) {// Hides video in subscriptions section
+                        $(this).closest("ytd-rich-item-renderer").find("button.style-scope.yt-icon-button").click();
+                        setTimeout(function(){
+                            $("yt-formatted-string:contains('Hide')").click();
+                        }, 50);
+                    }
+                    else if(e.altKey) { // Stops all videos from that channel getting onto your homepage
                         $(this).closest("ytd-rich-item-renderer").find("button.style-scope.yt-icon-button").click();
                         setTimeout(function(){
                             $(`yt-formatted-string:contains("Don't recommend channel")`).click();
