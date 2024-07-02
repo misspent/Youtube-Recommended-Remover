82c72545b7dbb758db4ceee2b5070810e5b03b5b
misspent <78914154+misspent@users.noreply.github.com>
2024-07-02 14:36:07 +0000

    Completely re-wrote | Not perfect

--- main.js
+++ main.js
@@ -1,13 +1,161 @@
 // ==UserScript==
 // @name         I'm not interested in this fucking YouTube video!
 // @version      2.0
-// @description  Removes videos from home page and hides videos from subscriptiion page when you press the allocated keybind
+// @description  Actions menu shortcuts: hover over a video and press one of the keybinds, and it'll do that action. This is also very organized, so it should be easy to add your own/ modifications if you want more control.
 // @namespace    0x7FFFFFFFFFFFFFFF & Misspent
-// @icon         https://www.youtube.com/s/desktop/03f86491/img/favicon.ico
+// @icon         https://i.imgur.com/vXIK1WF.png
 // @match        *://www.youtube.com/*
-// @run-at       document-end
+// @run-at       document-body
 // @require      https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js
 // ==/UserScript==
+
+
+(function() {
+    'use strict';
+
+    const checkInterval = 1000; // Reduced interval for faster checks (in milliseconds)
+    const hoverDelay = 50; // Delay before clicking the action buttons
+    const debounceTime = 100; // Debounce time for hover events
+
+    let checkedFlags = {
+        home: false,
+        watch: false,
+        subscriptions: false,
+        playlist: false
+    };
+
+    const pageSelectors = {
+        home: {
+            cellSelector: "ytd-rich-item-renderer.style-scope.ytd-rich-grid-row:not(.Not-Interested)",
+            itemSelector: "ytd-rich-item-renderer"
+        },
+        watch: {
+            cellSelector: "ytd-compact-video-renderer.style-scope.ytd-item-section-renderer:not(.Not-Interested)",
+            itemSelector: "ytd-compact-video-renderer"
+        },
+        subscriptions: {
+            cellSelector: "ytd-rich-item-renderer.style-scope.ytd-rich-grid-row:not(.Not-Interested)",
+            itemSelector: "ytd-rich-item-renderer"
+        },
+        playlist: {
+            cellSelector: "ytd-playlist-video-renderer.style-scope.ytd-playlist-video-list-renderer:not(.Not-Interested)",
+            itemSelector: "ytd-playlist-video-renderer"
+        }
+    };
+
+    const actions = {
+        home: {
+            shiftKey: 'Not interested',
+            altKey: "Save to Watch later"
+        },
+        watch: {
+            shiftKey: 'Not interested',
+            altKey: "Save to Watch later"
+        },
+        subscriptions: {
+            shiftKey: 'Hide',
+            altKey: "Don't recommend channel"
+        },
+        playlist: {
+            shiftKey: 'Remove from',
+            altKey: "Don't recommend channel"
+        }
+    };
+
+    function setupHoverHandlers(pageType, cellSelector, itemSelector) {
+        $(cellSelector).each(function() {
+            const cell = $(this);
+            const hoverElement = pageType === 'playlist' ? cell : cell.find("ytd-thumbnail");
+
+            hoverElement.hover(debounce(function(e) {
+                Object.keys(actions[pageType]).forEach(key => {
+                    if (e[key]) {
+                        triggerAction(cell, itemSelector, actions[pageType][key]);
+                    }
+                });
+            }, debounceTime));
+        }).addClass("Not-Interested");
+    }
+
+    function triggerAction(cell, itemSelector, actionText) {
+        try {
+            const button = cell.closest(itemSelector).find("button.style-scope.yt-icon-button");
+            if (button.length) {
+                button.click();
+                setTimeout(function() {
+                    const actionButton = $(`yt-formatted-string:contains('${actionText}')`);
+                    if (actionButton.length) {
+                        actionButton.click();
+                    } else {
+                        console.error(`Action button "${actionText}" not found.`);
+                    }
+                }, hoverDelay);
+            } else {
+                console.error("Button not found.");
+            }
+        } catch (error) {
+            console.error("Error in triggerAction: ", error);
+        }
+    }
+
+    function debounce(func, wait) {
+        let timeout;
+        return function(...args) {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => func.apply(this, args), wait);
+        };
+    }
+
+    function checkCells(pageType) {
+        const { cellSelector, itemSelector } = pageSelectors[pageType];
+        const cells = $(cellSelector);
+        if (cells.length === 0) {
+            if (!checkedFlags[pageType]) {
+                console.log(`Can't find any cells to check on ${pageType} page`);
+                checkedFlags[pageType] = true;
+            }
+        } else {
+            console.log(`Found ${cells.length} cells on ${pageType} page`);
+            setupHoverHandlers(pageType, cells, itemSelector);
+            checkedFlags[pageType] = false; // Reset the flag when cells are found
+        }
+    }
+
+    function detectPage() {
+        if (/youtube\.com\/?$/.test(location.href)) {
+            return 'home';
+        } else if (/youtube\.com\/watch/.test(location.href)) {
+            return 'watch';
+        } else if (/youtube\.com\/feed\/subscriptions/.test(location.href)) {
+            return 'subscriptions';
+        } else if (/youtube\.com\/playlist/.test(location.href)) {
+            return 'playlist';
+        } else {
+            return null;
+        }
+    }
+
+    function init() {
+        setInterval(function() {
+            const currentPage = detectPage();
+            if (currentPage) {
+                checkCells(currentPage);
+            }
+        }, checkInterval);
+    }
+
+    $(document).ready(init);
+})();
+
+
+
+
+
+
+
+
+/* Old:
+
 
 (function() {
     'use strict';
@@ -53,3 +201,6 @@
         }), 2000);
     }
 })();
+
+
+*/
